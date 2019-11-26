2ed8411b78702cdff3f29dc13c066d0be93e8028
世佳 <leejiawang@live.com>
2019-11-26 18:49:38 +0800

    update

--- en.search.min.1c5e3e8a01209a66b7f256f011a89c483186f27ae1b6444183c1568736217d9f.js
+++
@@ -1,5 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.fb6baae576576ef45594e443494f239836c462fe6349bd303d3f70744808b409.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){return;}
-const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
